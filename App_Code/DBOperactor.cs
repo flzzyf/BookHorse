0acc8015f6a4cff612840955fdd11c2b0f1ad811
Flzzyf <2981019122@qq.com>
2018-04-08 12:04:37 +0800

    数据库操作简化未完成

---
+++ App_Code/DBOperactor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+/// <summary>
+/// DBOperactor 的摘要说明
+/// </summary>
+public class DBOperactor
+{
+    public static DBOperactor instance;
+
+    public static void Init()
+    {
+        instance = new DBOperactor();
+    }
+    public DBOperactor()
+    {
+
+    }
+    public List<List<Object>> Search(string _s, string _tbName, string _columnName)
+    {
+        DBConnect.StartConnection();
+
+        string sql = "select * from " + _tbName + " where " + _columnName + " like '%" + _s + "%'";
+
+        SqlDataReader dr = DBConnect.ExecuteSql(sql);
+
+        List<List<Object>> list = new List<List<object>>();
+
+        int a = 0;
+
+        while (dr.Read())
+        {
+            Console.WriteLine(dr.Depth);
+
+            for (int i = 0; i < dr.FieldCount; i++)
+            {
+                list[a][i] = dr.GetValue(i);
+
+            }
+
+            a++;
+
+        }
+
+        dr.Close();
+        DBConnect.EndConnection();
+
+        return list;
+    }
+}
--- Web/DBTest.aspx.cs
+++ Web/DBTest.aspx.cs
@@ -10,38 +10,32 @@ public partial class Web_DBTest : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        
+        DBOperactor.Init();
     }
 
     protected void bt_search_Click(object sender, EventArgs e)
     {
         string s = tb_search.Text;
 
-        Search(s);
+        //Search(s);
+        //Insert(s);
+
+        //Label1.Text = s;
+        //Search(s);
+
+        List<List<Object>> list = DBOperactor.instance.Search("z", "Book2", "name");
+        Label1.Text = list[0][0].ToString();
     }
 
-    void Search(string _s)
+    void Insert(string _s)
     {
         DBConnect.StartConnection();
 
-        string sql = "select * from Book where id like '%" + _s + "%'";
+        string sql = "insert into Book2 (name) values ('" + _s + "')";
+        DBConnect.ExecuteSql(sql);
 
-        SqlDataReader dr = DBConnect.ExecuteSql(sql);
-
-        string s = "";
-        while (dr.Read())
-        {
-            s += dr.GetInt32(0);
-            s += " ";
-
-            s += dr.GetString(1);
-            s += "</br>";
-
-        }
-
-        Label1.Text = s;
-
-        dr.Close();
         DBConnect.EndConnection();
     }
+
+
 }
